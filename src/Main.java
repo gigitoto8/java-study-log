af137cadb5d6256d3ee316c172ea6f8c72b82454
gigitoto8 <gradu2a8nge@aol.com>
2026-03-22 16:14:12 +0900

    コンソール入力

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("日付を入力　：　");
+        String date = sc.nextLine();
+
+        System.out.print("科目を入力　：　");
+        String subject = sc.nextLine();
+
+        System.out.print("学習時間（分）を入力　：　");
+        int minutes = Integer.parseInt(sc.nextLine());
+
+        System.out.print("メモ　：　");
+        String memo = sc.nextLine();
+
         StudyRecord record = new StudyRecord(
-            "2026-03-18", "java", 120, "CSV出力テスト");
+            date,subject, minutes, memo);
         CsvService service = new CsvService();
-        service.save(record);    
-        }
+        service.save(record);
+        
+        sc.close();
+    }
 }
